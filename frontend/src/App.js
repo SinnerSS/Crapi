c73a318b0f04dcdbe65622d3545ea8fb4b143af2
SinnerSS <ntnbusinf@gmail.com>
2024-01-09 20:49:05 +0700

    chore: add font and icon for mui Signed-off-by: SinnerSS <ntnbusinf@gmail.com>
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CurrencyConverterPage from './pages/CurrencyConverterPage';
