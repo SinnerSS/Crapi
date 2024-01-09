4fb89a9c9fe26483b2d1fb0f711937ff209a1336
SinnerSS <ntnbusinf@gmail.com>
2024-01-10 03:48:35 +0700

    feat: add animation for button on hover Signed-off-by: SinnerSS <ntnbusinf@gmail.com>
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Logo from '../images/logo.png'
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Logo from '../images/logo.png';
+
+const buttonTheme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          position: 'relative',
+          marginLeft: '30px',
+          textTransform: 'none',
+          fontSize: '30px',
+          fontWeight: 300,
+          overflow: 'hidden',
+          '&::after': {
+            content: '""',
+            position: 'absolute',
+            width: '0%',
+            height: '3px',
+            bottom: '0',
+            left: '50%',
+            backgroundColor: '#FFFFFF',
+            transform: 'translate(-50%)',
+            transition: 'width .3s ease-in-out',
+          },
+          '&:hover::after': {
+            width: '100%',
+          },
+        },
+      },
+    },
+  },
+ });
 
 function NavBar() {
   return (
@@ -11,9 +43,11 @@ function NavBar() {
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <img src={Logo} alt='Logo' style={{ height: '40px' }} />
-          <Button component={Link} to="/" color="inherit" style={{ marginLeft: '30px', textTransform: 'none', fontSize: '30px', fontWeight: 300}}>
-            Converter
-          </Button>
+          <ThemeProvider theme={buttonTheme}>
+            <Button component={Link} to="/" color="inherit">
+              Converter
+            </Button>
+          </ThemeProvider>
         </div>
       </Toolbar>
     </AppBar>
