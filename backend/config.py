6f7c536fb39a28846ff404fd119c9a890912f311
SinnerSS <ntnbusinf@gmail.com>
2024-01-10 22:21:55 +0700

    add fixer api url Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 class Config:
   EXCHANGE_RATE_URL = "http://api.exchangeratesapi.io/v1/latest"
+  FIXER_URL = "http://data.fixer.io/api/"
   VIETCOMBANK_RATE_URL = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx"
   TECHCOMBANK_RATE_URL = "https://techcombank.com/api/data/exchange-rates?inputDate_eq=2023-12-15&_sort=inputTime:DESC"
