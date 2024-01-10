5288e1d88239fe77341466c5db1e5a085d904ae4
SinnerSS <ntnbusinf@gmail.com>
2024-01-10 13:48:15 +0700

    feat: add handler for techcombank Signed-off-by: SinnerSS <ntnbusinf@gmail.com>
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
   EXCHANGE_RATE_URL = "http://api.exchangeratesapi.io/v1/latest"
   VIETCOMBANK_RATE_URL = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx"
-  TECHCOMBANK_RATE_URL = " https://techcombank.com/api/data/exchange-rates?inputDate_eq=2023-12-15&_sort=inputTime:DESC"
+  TECHCOMBANK_RATE_URL = "https://techcombank.com/api/data/exchange-rates?inputDate_eq=2023-12-15&_sort=inputTime:DESC"
