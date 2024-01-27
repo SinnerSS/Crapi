8ab920ccc5d8a1d515019f9b7afcbb3c3a25ca3e
SinnerSS <102637959+SinnerSS@users.noreply.github.com>
2024-01-27 20:30:26 +0700

    feat: add time step selection to trend chart (#4)
    * add fetch historical rate to exchange rate handler
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>
    
    * add time step selection to trend chart
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- backend/app/api/exchange_rate_handler.py
+++ backend/app/api/exchange_rate_handler.py
@@ -1,15 +1,15 @@
 import requests, os
-from requests_cache import SQLiteCache, CachedSession 
+from requests_cache import SQLiteCache, CachedSession , NEVER_EXPIRE
 from config import Config
 
-backend = SQLiteCache('cache/exchange_rate_cache')
-session = CachedSession(backend=backend, timeout=3600)
+convert_backend = SQLiteCache('cache/exchange_rate_convert_cache')
+convert_session = CachedSession(backend=convert_backend, timeout=3600)
 
 def fetch_exchange_rate(base_currency, target_currency) :
   url = Config.EXCHANGE_RATE_URL + f"latest?access_key={os.environ.get('EXCHANGE_RATE_API_KEY')}"
 
   try:
-    response = session.get(url)
+    response = convert_session.get(url)
 
     data = response.json()
     data['rates']['EUR'] = 1
@@ -23,11 +23,30 @@ def fetch_exchange_rate(base_currency, target_currency) :
 
   return None
 
+trend_backend = SQLiteCache('cache/exchange_rate_trend_cache')
+trend_database = CachedSession(backend=trend_backend, timeout=NEVER_EXPIRE)
+
+def fetch_historical_rate(date, currency) :
+  url = Config.EXCHANGE_RATE_URL + f"{date}?access_key={os.environ.get('EXCHANGE_RATE_API_KEY')}"
+
+  try:
+    response = trend_database.get(url)
+
+    data = response.json()
+
+    return data['rates']['VND'] / data['rates'][currency]
+    
+  except RequestException as e:
+    print(f"Request Exception: {e}")
+    return None
+
+  return None
+
 def fetch_symbol_list() :
   url = Config.EXCHANGE_RATE_URL + f"symbols?access_key={os.environ.get('EXCHANGE_RATE_API_KEY')}"
 
   try:
-    response = session.get(url)
+    response = convert_session.get(url)
 
     data = response.json()
 
